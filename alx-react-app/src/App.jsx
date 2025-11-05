ac61552bed17640d79edc61d6a1e98013feba349
Precious Ayigbe <preciousayigbe96@gmail.com>
2025-11-05 16:40:29 +0100

    third react component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,23 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <WelcomeMessage />
+      <Footer />
 
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>tag text to “Hello everyone, I am learning React at ALX!</h1>
+
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +36,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
