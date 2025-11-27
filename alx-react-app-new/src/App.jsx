fb237396d74ad30ab26caf82c29398b564a2bb3d
Precious Ayigbe <preciousayigbe96@gmail.com>
2025-11-27 18:33:06 +0100

    refix 2

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +17,7 @@ function App() {
       <MainContent />
       <WelcomeMessage />
       <Footer />
-      
+      <Counter /> 
       <div>
       <h1>User Profile Card</h1>
       <UserProfile 
