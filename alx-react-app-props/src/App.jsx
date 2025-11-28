4a58d4781f674b31034eb4abb5e7d844b75963f9
Precious Ayigbe <preciousayigbe96@gmail.com>
2025-11-28 10:53:04 +0100

    refactored 3

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 import UserProfile from "./components/UserProfile";
 import Counter from "./components/Counter";
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
 function App() {
   const [count, setCount] = useState(0)
 const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
