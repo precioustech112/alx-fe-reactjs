d68330163314a7f3796e44a93145149460789238
Precious Ayigbe <preciousayigbe96@gmail.com>
2025-11-28 10:56:48 +0100

    refactored 4

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,19 +1,13 @@
-function UserProfile(props) {
+import  { useContext } from "react";
+import UserContext from "../UserContext";
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div
-      style={{
-        border: "1px solid gray",
-        padding: "10px",
-        margin: "10px",
-        borderRadius: "8px",
-        backgroundColor: "#f2f2f2"
-      }}
-    >
-      <h2 style={{ color: "blue", marginBottom: "5px" }}>{props.name}</h2>
-      <p style={{ margin: "4px 0" }}>
-        Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
-      </p>
-      <p style={{ margin: "4px 0" }}>Bio: {props.bio}</p>
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
